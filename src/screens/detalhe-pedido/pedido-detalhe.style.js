2deab2eecf4183302a77a39516e3d89c3ee24a43
giovaniCheloni <130862039+giovaniCheloni@users.noreply.github.com>
2025-01-06 17:10:56 -0300

    add Detalhe-pedido e detalhe-produto

---
+++ src/screens/detalhe-pedido/pedido-detalhe.style.js
@@ -0,0 +1,45 @@
+import { COLORS, FONT_SIZE } from "../../constants/theme"
+
+export const styles = {
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.white,
+        padding: 20,
+        paddingTop: 50
+    },
+    header: {
+        width: "100%",
+        height: 40,
+        justifyContent: "center",
+        marginBottom: 50
+    },
+    containerBack: {
+        position: "absolute",
+        top: 7,
+        left: 0
+    },
+    back: {
+        width: 30,
+        height: 30
+    },
+    titulo: {
+        fontSize: FONT_SIZE.lg,
+        color: COLORS.dark_gray,
+        textAlign: "center"
+    },
+    valores: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 4,
+        marginBottom: 4
+    },
+    valor: {
+        fontSize: FONT_SIZE.sm,
+        color: COLORS.dark_gray
+    },
+    total: {
+        fontSize: FONT_SIZE.sm,
+        color: COLORS.dark_gray,
+        fontWeight: "bold"
+    }
+}
